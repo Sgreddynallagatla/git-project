e8195127a9237cff905732e70cb5b785e7562d8c
Sgreddynallagatla <nallagatlasivagovindareddy@gmail.com>
2024-07-17 10:23:33 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'git-app';
+
+
+  usersData=[
+    {
+      "id":1,
+      "name":"Leanne Graham",
+      "username":"Bret",
+      "email":"Sincere@april.biz",
+    },
+    {
+      "id":2,
+      "name":"Ervin Howell",
+      "username":"Antonette",
+      "email":"Shanna@melissa.tv",
+    },
+    {
+      "id":3,
+      "name":"Clementine Bauch",
+      "username":"Samantha",
+      "email":"Nathan@yesenia.net",
+    }
+  ]
 }
